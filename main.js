3342b486ecd4a30f4d7541b5a438af0070676b5e
Shreyas <72331003+Shreyas94@users.noreply.github.com>
2021-06-03 12:06:32 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,14 +1,16 @@
 song = "";
+song2 = "";
 leftWristX = 0;
 leftWristY = 0;
 rightWristX = 0;
 rightWristY = 0;
 
-function preload(){
+function preload() {
     song = loadSound("music.mp3");
+    song2 = loadSound("music2.mp3")
 }
 
-function setup(){
+function setup() {
     canvas = createCanvas(600, 500);
     canvas.center();
 
@@ -19,42 +21,58 @@ function setup(){
     poseNet.on('pose', gotPoses);
 }
 
-function modelLoaded(){
+function modelLoaded() {
     console.log('PoseNet Is Initialized');
 }
 
-function draw(){
+function draw() {
     image(video, 0, 0, 600, 500);
     fill("FF0000");
-    stroke("FF0000"); 
-   
-    if(scoreleftWrist > 0.2){  
-    circle(leftWristX, leftWristY, 20);
-    InNumberleftWristY = Number(leftWristY);
-    romove_decimals = floor(InNumberleftWristY);
-    volume = remove_decimals/500;
-    document.getElementById("volume").innerHTML = "VOlume = " + volume;      
-    song.setVolume(volume);
-}
-}
+    stroke("FF0000");
 
-function play(){
+
+    if (scoreLeftWrist > 0.2) {
+        circle(leftWristX, leftWristY, 20);
+        InNumberleftWristY = Number(leftWristY);
+        romove_decimals = floor(InNumberleftWristY);
+        volume = remove_decimals / 500;
+        document.getElementById("song_name").innerHTML = "Song Name = PeterPan";
+        song.play();
+    }    
+
+    
+    if (scoreRightWrist > 0.2) {
+        circle(rightWristX, rightWristY, 20);
+        InNumberrightWristY = Number(rightWristY);
+        romove_decimals = floor(InNumberrighttWristY);
+        volume = remove_decimals / 500;
+        document.getElementById("song_name").innerHTML = " Song Name = Harry Potter Theme Song";
+        song2.play();
+    }    
+    }
+
+
+
+function play() {
     song.play();
     song.setVolume(1);
     song.rate(1);
 }
 
-function gotPoses(result){
-    if (result.length > 0){
+function gotPoses(result) {
+    if (result.length > 0) {
         console.log(result);
         scoreLeftWrist = result[0].pose.keypoints[9].score;
-        console.log("ScoreLeftWrist = " + scoreLeftWrist);
+        scoreRightWrist = result[0].pose.keypoints[10].score;
+        console.log("ScoreRightWrist = " + scoreRIghtWrist + "ScoreLeftWrist = " + scoreLeftWrist);
+
         leftWristX = result[0].pose.leftWrist.x;
         leftWristY = result[0].pose.leftWrist.y;
-        console.log("LeftWristX = " + leftWristX + "LeftWristY = " +leftWristY); 
+        console.log("LeftWristX = " + leftWristX + "LeftWristY = " + leftWristY);
+
         rightWristX = result[0].pose.rightWrist.x;
-        rightWristY = result[0].pose.rightWrist.y;    
-        console.log("rightWristX = " + rightWristX + "RightWristY = " +rightWristY); 
-    
-    }       
+        rightWristY = result[0].pose.rightWrist.y;
+        console.log("rightWristX = " + rightWristX + "RightWristY = " + rightWristY);
+
+    }
 }
